adbcf3823a0baa7f9e7b02a5fe0638004a129eef
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-07-19 01:50:51 +0200

    brush up example

--- examples/demo.rs
+++ examples/demo.rs
@@ -5,19 +5,18 @@ use man::Man;
 fn main() {
   let page = Man::new("basic")
     .description("perform basic operations")
-    .author(&"Alice Person", Some(String::from("alice@person.com")))
-    .author("Bob Human", Some(String::from("bob@human.com")))
+    .author("Alice Person", Some("alice@person.com".into()))
+    .author("Bob Human", Some("bob@human.com".into()))
     .flag(
       Some("-h".into()),
       Some("--help".into()),
-      Some("Print help information.".into()),
+      Some("Prints help information.".into()),
     )
     .flag(
       Some("-v".into()),
       Some("--verbose".into()),
       Some("Prints version information.".into()),
     );
-  // .flag(Some("-v"), Some("--verbose"), Some("Verbose mode"));
   // .option(Some("-o"), Some("--output"), "output", None, "Output file");
 
   println!("{}", page.render());
